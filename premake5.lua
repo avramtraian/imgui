76484753edf866a4beddb779f79d4aead2f249d6
avramtraian <69006162+avramtraian@users.noreply.github.com>
2021-06-21 15:39:31 +0300

    Updated the premake5.lua file to FreezaEngine requirments

--- premake5.lua
+++ premake5.lua
@@ -2,21 +2,27 @@ project "ImGui"
 	kind "StaticLib"
 	language "C++"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("Engine/Binaries/Win64")
+	objdir ("Engine/Intermediate/Win64")
 
 	files
 	{
 		"imconfig.h",
 		"imgui.h",
 		"imgui.cpp",
+		"imgui_demo.cpp",
 		"imgui_draw.cpp",
+		"imgui_tables.cpp",
 		"imgui_internal.h",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_impl_dx11.h",
+		"imgui_impl_dx11.cpp",
+		"imgui_impl_win32.h",
+		"imgui_impl_win32.cpp"
+
 	}
 
 	filter "system:windows"
@@ -30,10 +36,19 @@ project "ImGui"
 		cppdialect "C++17"
 		staticruntime "On"
 
-	filter "configurations:Debug"
+	filter "configurations:Debug_Game"
 		runtime "Debug"
 		symbols "on"
 
-	filter "configurations:Release"
+	filter "configurations:Debug_Editor"
+		runtime "Debug"
+		symbols "on"
+
+
+	filter "configurations:Development_Game"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Development_Editor"
 		runtime "Release"
 		optimize "on"
